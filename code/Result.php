4bf981438dbc10b92a89e16efa3cadbd5e3cd95a
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:03:24 +1300

    Converted to PSR-2

--- code/Result.php
+++ code/Result.php
@@ -1,20 +1,22 @@
 <?php
 
-class groupdocsJavaViewer_Result extends ViewableData {
-	protected $width = false;
-	protected $height = false;    
-	protected $server_url = false;
+class groupdocsJavaViewer_Result extends ViewableData
+{
+    protected $width = false;
+    protected $height = false;
+    protected $server_url = false;
 
-	
-	public function __construct($width, $height, $url) {
-		$this->width = $width;
-		$this->height = $height;
-		$this->server_url = $url;
-		parent::__construct();
-	}
-	
-	public function forTemplate() {
-		return "<iframe src='{$this->server_url}' frameborder='0' width='" . $this->width . "' height='" . $this->height ."'></iframe>";
-	}
-	
+    
+    public function __construct($width, $height, $url)
+    {
+        $this->width = $width;
+        $this->height = $height;
+        $this->server_url = $url;
+        parent::__construct();
+    }
+    
+    public function forTemplate()
+    {
+        return "<iframe src='{$this->server_url}' frameborder='0' width='" . $this->width . "' height='" . $this->height ."'></iframe>";
+    }
 }
